6cadacc170f45ecfda54f6db796b517620dbda83
gadai anish <mrgadai6077@gamail.com>
2025-11-26 15:39:28 +0530

    rename file resultListCode.php

--- assest/js/result.js
+++ assest/js/result.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         info: true,
         pageLength: 10,
         ajax: {
-            url: 'result_list-code.php',
+            url: 'resultListCode.php.php',
             type: 'POST',
             dataSrc: '',
         },
@@ -54,7 +54,7 @@ $(document).ready(function () {
         const Id = $(this).data('id');
         const view = "view";
         $.ajax({
-            url: 'result_list-code.php',
+            url: 'resultListCode.php.php',
             method: 'POST',
             data: { id: Id, view: view },
             dataType: "json",
@@ -78,7 +78,7 @@ $(document).ready(function () {
         $('#confirmModal').modal('show');
         $('#delete').on('click', function () {
             $.ajax({
-                url: 'result_list-code.php',
+                url: 'resultListCode.php.php',
                 method: 'POST',
                 data: { id: Id, del: del },
                 success: function (response) {
@@ -149,7 +149,7 @@ $(document).ready(function () {
             var addformData = new FormData(this);
             addformData.append('add', 'add');
             $.ajax({
-                url: 'result_list-code.php',
+                url: 'resultListCode.php.php',
                 method: 'POST',
                 data: addformData,
                 processData: false,
@@ -187,7 +187,7 @@ $(document).ready(function () {
         const Id = $(this).data('id');
         const view = "view";
         $.ajax({
-            url: 'result_list-code.php',
+            url: 'resultListCode.php.php',
             method: 'POST',
             data: { id: Id, view: view },
             success: function (response) {
